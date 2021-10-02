9b594136649d3bb3c023812f5df34d8b5fec55ab
saving714 <ivanoffffff01@gmail.com>
2021-10-02 12:58:55 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="character-builder">
+    <CharacterAttributeChanger 
+      v-for="( value, key) in characterParams"
+      :key="key"
+      :value="characterParams[key].val"
+      :additio="characterParams[key].addition"
+      :maxCoun="characterParams[key].maxCount"
+      @change="(v) => { handleChangeCharacter(key, v) }" 
+    />    
+  </div>
+</template>
+
+<script>
+import CharacterAttributeChanger from '@/components/character_attribute_changer.vue'
+
+export default {
+  components: {
+    CharacterAttributeChanger
+  },
+  data: () => ({
+    characterParams: [
+    {id: 0, val: 0, maxCount: 45, addition: 1},
+    {id: 2, val: 0, maxCount: 45, addition: 1 },
+    {id: 3, val: 0, maxCount: 45, addition: 1},
+    {id: 4, val: 0, maxCount: 45, addition: 1},
+    {id: 5, val: 0, maxCount: 1.0, addition: 0.1},
+    {id: 6, val: 0, maxCount: 1.0, addition: 0.1}]
+    
+  }),
+   methods: {
+    handleChangeCharacter(key, v) {
+      if (v > this.characterParams[key].maxCount || v < 0) {return}
+      this.characterParams[key].val = v
+      alt.emit('Red', this.characterParams)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.character-builder {
+    max-width: 100%;
+    height: 600px;
+    width: 350px;
+    background: linear-gradient(0deg, rgb(0, 0, 0) 0%, rgb(61, 61, 61) 100%);    
+    border-radius: 7%;
+    opacity: 0.95; 
+}
+</style>
---
+++ src/components/character_attribute_changer.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="character-attribute-changer">
+    <button @click="$emit('change', +(value - additio).toFixed(1))">←</button>
+    <div> {{value}} | {{maxCoun}} | {{ additio }} </div>
+    <button @click="$emit('change',+(value + additio).toFixed(1))">→</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Number,
+      required: true
+    },
+    maxCoun: {
+      type: Number,
+      required: true
+    },
+    additio: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script> 
+
+<style scoped>
+:root {
+  --background-color: #000000;
+  --color: #000000;
+  --light-theme: #ffffff;
+}
+.character-attribute-changer {
+  display: flex;
+  justify-content: space-around;
+  padding-top: 20px;
+  color: white;
+}
+button {
+  color: white;
+  border-radius: 50%;
+}
+button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  background-color: var(--light-theme);
+  cursor: pointer;
+  font-size: 24px;
+  color: var(--color);
+  transition: all 0.3s;
+  position: relative;
+
+  text-align: center;
+  overflow: hidden;
+
+  border-radius: 45%;
+  box-shadow: 0 6px 30px -10px rgba(#CCCCCC, 1);
+}
+button:hover {
+  transform: translateX(5px) translateY(-7px);
+}
+
+</style>
